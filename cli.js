1e9c2afddfd078d2305822d947990160760eb28a
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2024-10-05 20:16:21 -0300

    Instalando e implementando a lib externa chalk para decorar infos do terminal e criação do gitignore

--- cli.js
+++ cli.js
@@ -6,6 +6,7 @@ import { Command } from 'commander';
 import trataErros from './erros/funcoesErro.js';
 import { contaPalavras } from './index.js';
 import { montaSaidaArquivo } from './helpers.js';
+import chalk from 'chalk';
 
 const program = new Command();
 
@@ -17,7 +18,7 @@ program
     const { texto, destino } = options;
 
     if (!texto || !destino) {
-      console.error('erro: favor inserir caminho de origem e destino')
+      console.error(chalk.redBright('erro: favor inserir caminho de origem e destino'));
       program.help();
       return;
     }
@@ -27,7 +28,7 @@ program
 
     try {
       processaArquivo(caminhoTexto, caminhoDestino);
-      console.log('texto processado com sucesso');
+      console.log(chalk.greenBright('texto processado com sucesso'));
     } catch (erro) {
       console.log('ocorreu um erro no processamento', erro);
     }
@@ -54,7 +55,7 @@ async function criaESalvaArquivo(listaPalavras, endereco) {
   const textoPalavras = montaSaidaArquivo(listaPalavras);
   try {
     await fs.promises.writeFile(arquivoNovo, textoPalavras);
-    console.log('arquivo criado');
+    console.log(chalk.bgBlueBright('arquivo criado'));
   } catch(erro) {
     throw erro;
   }
